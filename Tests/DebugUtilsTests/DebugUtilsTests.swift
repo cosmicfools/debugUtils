2bb3ca9a9acca9e15f603f63e3ab6aa663b0f3c1
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-03-14 23:28:50 +0100

    Add CI to the project

---
+++ Tests/DebugUtilsTests/DebugUtilsTests.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Francisco Javier Trujillo Mata on 14/03/2022.
+//
+
+import XCTest
+@testable import DebugUtils
+
+final class DebugUtilsTests: XCTestCase {
+    func test_dummy() {
+        XCTAssert(true)
+    }
+}
